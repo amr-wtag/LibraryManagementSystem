127f670fb64f763c139d2678fafda2273175b90b
mridha <98802032+amr-wtag@users.noreply.github.com>
2025-01-09 15:57:49 +0600

    Created Book Modal

---
+++ Backend/Models/Book.cs
@@ -0,0 +1,11 @@
+
+public class Book
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Category { get; set; }
+    public string CopiesAvailable { get; set; }
+    
+    public ICollection<Transaction> Transactions { get; set; } 
+}
