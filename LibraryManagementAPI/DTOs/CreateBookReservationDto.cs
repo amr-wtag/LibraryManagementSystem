7253380d28817e083e40ca4fbe70b2e28ddc60f9
amr-wtag <ashraful.mridha@welldev.io>
2025-04-24 02:27:53 +0600

    added due date for each books for reservation

--- LibraryManagementAPI/DTOs/CreateBookReservationDto.cs
+++ LibraryManagementAPI/DTOs/CreateBookReservationDto.cs
@@ -3,6 +3,12 @@ namespace LibraryManagementAPI.DTOs;
 public class CreateBookReservationDto
 {
     public Guid UserId { get; set; }
-    public List<Guid> BookIds { get; set; } = new();
+
+    public List<BookReservationItemDto> Reservations { get; set; } = new();
+}
+
+public class BookReservationItemDto
+{
+    public Guid BookId { get; set; }
     public DateTime DueDate { get; set; }
 }
