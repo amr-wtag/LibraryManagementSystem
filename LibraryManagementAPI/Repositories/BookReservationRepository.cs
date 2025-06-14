c008eb4b5eed446600013509d77888e29aa5862a
amr-wtag <ashraful.mridha@welldev.io>
2025-06-14 11:56:38 +0600

    Made bookReservation filter from single to multiple

--- LibraryManagementAPI/Repositories/BookReservationRepository.cs
+++ LibraryManagementAPI/Repositories/BookReservationRepository.cs
@@ -14,27 +14,27 @@ public class BookReservationRepository : IBookReservationRepository
         _context = context;
     }
 
-    public async Task<List<BookReservation>> GetBookReservationsAsync(Guid? id = null, Guid? userId = null,
-        Guid? bookId = null, string status = "")
+    public async Task<List<BookReservation>> GetBookReservationsAsync(List<Guid>? ids = null, List<Guid>? userIds = null,
+        List<Guid>? bookIds = null, string status = "")
     {
         var query = _context.BookReservations
             .Include(r => r.Book)
             .Include(r => r.User)
             .AsQueryable();
 
-        if (id.HasValue)
+        if (ids != null && ids.Any())
         {
-            query = query.Where(r => r.Id == id.Value);
+            query = query.Where(r => ids.Contains(r.Id));
         }
 
-        if (userId.HasValue)
+        if (userIds != null && userIds.Any())
         {
-            query = query.Where(r => r.UserId == userId.Value);
+            query = query.Where(r => userIds.Contains(r.UserId));
         }
 
-        if (bookId.HasValue)
+        if (bookIds != null && bookIds.Any())
         {
-            query = query.Where(r => r.BookId == bookId.Value);
+            query = query.Where(r => bookIds.Contains(r.BookId));
         }
 
         if (status != "")
